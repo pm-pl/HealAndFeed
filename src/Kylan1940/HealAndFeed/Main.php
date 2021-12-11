2cbd67434c975e9c914caca4ac2ead4cfdf1547d
Kylan1940 <Kevingaming123copyright@gmail.com>
2021-12-12 03:07:18 +0700

    TEST-01

--- src/Kylan1940/HealAndFeed/Main.php
+++ src/Kylan1940/HealAndFeed/Main.php
@@ -9,9 +9,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\Config;
-use onebone\economyapi\EconomyAPI;
-use Kylan1940\HealAndFeed\Form\{Form, SimpleForm};
 
 class Main extends PluginBase implements Listener {
 
@@ -22,49 +21,49 @@ class Main extends PluginBase implements Listener {
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
         if($sender instanceof Player){
-          $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-          $moneyheal = ($this->getConfig()->get("money-heal"));
-          $moneyfeed = ($this->getConfig()->get("money-feed"));
-          $money = $economy->myMoney($sender);
-            if($sender->hasPermission("healandfeed-heal.command")){
+          // $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+          // $moneyheal = ($this->getConfig()->get("money-heal"));
+          // $moneyfeed = ($this->getConfig()->get("money-feed"));
+          // $money = $economy->myMoney($sender);
+            // if($sender->hasPermission("healandfeed-heal.command")){
                 if($cmd->getName() == "heal"){
-                  if($money >= $moneyheal){
-                    $economy->reduceMoney($sender, $moneyheal);
+                  // if($money >= $moneyheal){
+                    // $economy->reduceMoney($sender, $moneyheal);
                     $sender->setHealth($sender->getMaxHealth());
                     $sender->sendMessage($this->getConfig()->get("message-heal"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
-                  }
+                  // } else {
+                    // $sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
+                  // }
                 } 
-            } else {
-              $sender->sendMessage($this->getConfig()->get("no-permission"));
-            }
-            if($sender->hasPermission("healandfeed-feed.command")){
+            // } else {
+              // $sender->sendMessage($this->getConfig()->get("no-permission-heal"));
+            // }
+            // if($sender->hasPermission("healandfeed-feed.command")){
                 if($cmd->getName() == "feed"){
-                  if($money >= $moneyfeed){
-                    $economy->reduceMoney($sender, $moneyfeed);
+                  // if($money >= $moneyfeed){
+                    // $economy->reduceMoney($sender, $moneyfeed);
                     $sender->setFood(20);
                     $sender->setSaturation(20);
                     $sender->sendMessage($this->getConfig()->get("message-feed"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
-                  }
+                  // } else {
+                    // $sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
+                  // }
                 } 
-            } else {
-              $sender->sendMessage($this->getConfig()->get("no-permission"));
-            }
-            if($sender->hasPermission("healandfeed-ui.command")){
-              if($cmd->getName() == "healfeed"){
-                $this->HealFeed($sender);
-              }
-            } else {
-              $sender->sendMessage($this->getConfig()->get("no-permission"));
-            }
+            // } else {
+              // $sender->sendMessage($this->getConfig()->get("no-permission-feed"));
+            // }
+            // if($sender->hasPermission("healandfeed-ui.command")){
+              // if($cmd->getName() == "healfeed"){
+                // $this->HealFeed($sender);
+              // } 
+            // } else {
+              // $sender->sendMessage($this->getConfig()->get("no-permission-ui"));
+            // }
         }
         return true;
     }
     
-  public function HealFeed($sender){
+  /* public function HealFeed($sender){
         $form = new SimpleForm(function (Player $sender, int $data = null){
             $result = $data;
             if ($result === null) {
@@ -72,29 +71,29 @@ class Main extends PluginBase implements Listener {
             }
             switch ($result) {
                 case 0:
-                  $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-                  $money = $economy->myMoney($sender); 
-                  $moneyheal = ($this->getConfig()->get("money-heal"));
-                  if($money >= $moneyheal){
-                    $economy->reduceMoney($sender, $moneyheal);
+                  // $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+                  // $money = $economy->myMoney($sender); 
+                  // $moneyheal = ($this->getConfig()->get("money-heal"));
+                  // if($money >= $moneyheal){
+                    // $economy->reduceMoney($sender, $moneyheal);
                     $sender->setHealth($sender->getMaxHealth());
                     $sender->sendMessage($this->getConfig()->get("message-heal"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
-                  }
+                  // } else {
+                    // $sender->sendMessage($this->getConfig()->get("not-enough-money-heal"));
+                  // }
                   break;
                 case 1:
-                  $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-                  $money = $economy->myMoney($sender);
-                  $moneyfeed = ($this->getConfig()->get("money-feed"));
-                    if($money >= $moneyfeed){
-                    $economy->reduceMoney($sender, $moneyfeed);
+                  // $economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+                  // $money = $economy->myMoney($sender);
+                  // $moneyfeed = ($this->getConfig()->get("money-feed"));
+                    // if($money >= $moneyfeed){
+                    // $economy->reduceMoney($sender, $moneyfeed);
                     $sender->setFood(20);
                     $sender->setSaturation(20);
                     $sender->sendMessage($this->getConfig()->get("message-feed"));
-                  } else {
-                    $sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
-                  }
+                  //} else {
+                    // $sender->sendMessage($this->getConfig()->get("not-enough-money-feed"));
+                  // }
                   break;
             }
         });
@@ -103,5 +102,5 @@ class Main extends PluginBase implements Listener {
             $form->addButton($this->getConfig()->get("button-feed"));
             $form->sendToPlayer($sender);
             return $form;
-    }
+    }*/
 }
