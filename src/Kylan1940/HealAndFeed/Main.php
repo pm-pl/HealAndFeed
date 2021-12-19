6d988e3e777c0be72ac33ef3807d7078c2795315
Kylan1940 <Kevingaming123copyright@gmail.com>
2021-12-19 11:16:32 +0700

    TEST-05

--- src/Kylan1940/HealAndFeed/Main.php
+++ src/Kylan1940/HealAndFeed/Main.php
@@ -1,36 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Kylan1940\HealAndFeed;
 
-use pocketmine\Server;
-use pocketmine\Player;
-use pocketmine\plugin\Plugin;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\command\ConsoleCommandSender;
-use pocketmine\utils\Config;
+use pocketmine\event\Listener;
 
-class Main extends PluginBase implements Listener {
-
-   public function onEnable() : void {
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable() : void {
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
         @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
-   }
-
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
-        if($sender instanceof Player){
-                if($cmd->getName() == "heal"){
-                    $sender->setHealth($sender->getMaxHealth());
-                    $sender->sendMessage($this->getConfig()->get("message-heal"));
-                } 
-                if($cmd->getName() == "feed"){
-                    $sender->setFood(20);
-                    $sender->setSaturation(20);
-                    $sender->sendMessage($this->getConfig()->get("message-feed"));
-                } 
-        }
-        return true;
-    }
+      }
+	
+  public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) : bool{
+           if ($sender instanceof Player) {
+               if ($cmd->getName() == "heal") {
+                       $sender->setHealth($sender->getMaxHealth());
+                       $sender->sendMessage($this->getConfig()->get("message-heal")); 
+               }
+              if ($cmd->getName() == "feed") {
+                       $sender->getHungerManager()->setFood(20);
+                       $sender->getHungerManager()->setSaturation(20);
+                       $sender->sendMessage($this->getConfig()->get("message-feed"));
+              }
+           } else {
+               $sender->sendMessage("§cYou must be in-game to use this command!");
+           }
+          return true;
+		}
 }
